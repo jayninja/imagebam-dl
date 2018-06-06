8766860fe02e8975e6758253440d27cf02ea28d2
James J <jnjhurani@uh.edu>
2018-06-06 11:33:01 -0500

    Create imagebam-dl.sh

---
+++ imagebam-dl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash/
+# usage : ./imagebam-dl.sh <URL>
+
+if [ -z "$1" ]
+then 
+  echo "usage : ./imagebam-dl.sh <URL>"
+  exit 1;
+fi
+
+DIRECTORY_NAME=`basename $1`
+mkdir -p ./$DIRECTORY_NAME
+
+for i in `curl -s "$1" |grep 'a href'| egrep -o "http.*" |grep /image/ | cut -d "'" -f1 | cut -d '"' -f1` ; 
+do 
+  DIRECT_IMAGE_URL=`curl -s "$i" | grep og:image | cut -f4 -d'"'`
+  FILENAME=`basename $DIRECT_IMAGE_URL`
+  wget -O ./$DIRECTORY_NAME/$FILENAME $DIRECT_IMAGE_URL 
+#FOLDER=$(echo $1 | cut -d '/' -f 5) ; curl -s "$i" | grep 'img id' | grep -o "http.*" | cut -d '"' -f1 | xargs wget -nv -nc -P imagebam_"$FOLDER"/ ; 
+done
